b3ee60ce54935e03df83649a2dc10bf230b3d7cb
nolbertog <nolberto.en@gmail.com>
2024-03-12 14:05:22 -0300

    message

---
+++ SampleFolder/Scope_lab.js
@@ -0,0 +1,26 @@
+//Global
+
+var globalVar = " soy una variale global";
+let globalLet = "Soy otra variable global con let";
+const globalConst = "soy una constante global";
+
+
+{
+    // block scope
+    var blockVar = "Soy un bloque-scope var";
+    let blockLet = "Soy un bloque-scope let";
+    const blockConst = "Soy un bblock-scope const"
+    
+}
+
+
+    // block scope
+    var blockVar = "Soy un bloque-";
+    let blockLet = "Soy un bloque-scope let";
+    const blockConst = "Soy un bblock-scope const"
+
+//Global scope
+
+alert(blockVar)
+alert(blockLet)
+alert(blockConst)
