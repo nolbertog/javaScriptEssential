91ace48b3f1d0afd8a17232acab94b9b36fa0a11
nolbertog <nolberto.en@gmail.com>
2024-03-13 10:10:01 -0300

    Bucle2

---
+++ SampleFolder/autenticado.js
@@ -0,0 +1,7 @@
+let autenticado = true;
+
+if (autenticado){
+    console.log("Autenticado")
+}else{
+    console.log("Nno autenticado")
+}
---
+++ SampleFolder/ternario.js
@@ -0,0 +1,5 @@
+let isAuthenticate = true;
+
+let isAuthenticateStatus = isAuthenticate ? "Autenticado": "no autenticado"
+
+console.log(isAuthenticateStatus)
